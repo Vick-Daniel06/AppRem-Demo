b5cc7285c55732bc0dd09354a65033bbd3644bc6
Daniel <danbbvacount25@gmail.com>
2026-08-19 13:09:04 -0600

    add DataSource usign models

--- apprem_v1/lib/Data/datasources/product_local_data_source.dart
+++ apprem_v1/lib/Data/datasources/product_local_data_source.dart
@@ -1,5 +1,5 @@
 import 'package:apprem_v1/Data/database/app_database.dart' as db;
-import 'package:apprem_v1/data/models/product_model.dart';
+import 'package:apprem_v1/Data/models/product_model.dart';
 
 class ProductLocalDataSource {
   final db.AppDatabase _database;
@@ -7,27 +7,18 @@ class ProductLocalDataSource {
   ProductLocalDataSource(this._database);
 /// QUERY: Trae todos los productos guardados en SQLite
   Future<List<ProductModel>> obtenerProductos() async{
+    
     final filas = await _database.select(_database.products).get();
+    final filasModel = filas.map((l) => ProductModel.fromDrift(l)).toList();
 
-    return filas.map((fila) => ProductModel(
-      id: fila.id,
-      nombre: fila.nombre,
-      pesoKg: fila.pesoKg,
-      precioSugerido: fila.precioSugerido,
-    )).toList();
+    return filasModel;
   }
 /// QUERY: Inserta un producto. Si el ID ya existe, actualiza los campos (Upsert)
   Future<void> guardarOActualizarProducto(ProductModel product) async{
-// Drift usa "Companions" para asegurar que los inserts lleven tipos válidos
-//Si el usuario edita un producto existente o agrega uno nuevo, la función detecta si el ID ya existe. Si existe, hace un UPDATE; si no, hace un INSERT.
-    await _database.into(_database.products).insertOnConflictUpdate(
-      db.ProductsCompanion.insert(
-        id: product.id,
-        nombre: product.nombre,
-        pesoKg: product.pesoKg,
-        precioSugerido: product.precioSugerido,
-      ),
-    );
+
+    await _database
+    .into(_database.products)
+    .insertOnConflictUpdate(product.toCompanion());
   }
 /// QUERY: Elimina un producto de la tabla mediante su UUID
   Future<void> eliminarProducto(String id) async{
--- apprem_v1/lib/Data/datasources/remission_local_data_source.dart
+++ apprem_v1/lib/Data/datasources/remission_local_data_source.dart
@@ -10,32 +10,26 @@ class RemissionLocalDataSource {
 /// QUERY: Trae todo el historial de remisiones con sus líneas de detalle cargadas
 Future<List<RemissionModel>> obtenerHistorialRemisiones() async {
     // Trae todas las cabeceras ordenadas por fecha (la más reciente primero)
-    final headerQuery = _database.select(_database.remissions)
-      ..orderBy([(t) => OrderingTerm(expression: t.fechaCreacion, mode: OrderingMode.desc)]);
+    /*final headerQuery = _database.select(_database.remissions)
+      ..orderBy([(t) => OrderingTerm(expression: t.fechaCreacion, mode: OrderingMode.desc)]);*/
     
-    final rawHeader = await headerQuery.get();
+    final rawHeader = await (_database.select(_database.remissions)
+    ..orderBy([(t)=> OrderingTerm(expression: t.fechaCreacion, mode: OrderingMode.desc)]))
+    .get()
+    ;
     final List<RemissionModel> history = [];
 
     // Por cada cabecera, buscamos sus renglones correspondientes
     for (var headers in rawHeader) {
-      final queryLineas = _database.select(_database.detailLines)
-        ..where((t) => t.remisionId.equals(headers.id));
-      
-      final rawLines = await queryLineas.get();
+      final rawLines = await (_database.select(_database.detailLines)
+      ..where((t)=> t.remisionId.equals(headers.id))).get();
 
       // Convierte las filas de las líneas a DetailLineModel
-      final lineasModel = rawLines.map((l) => DetailLineModel(
-        id: l.id,
-        remisionId: l.remisionId,
-        prodctoId: l.productoId,
-        nombreProducto: l.nombreProducto,
-        cantidad: l.cantidad,
-        precioSugeridoOriginal: l.precioSugeridoOriginal,
-        precioUnitarioEnEseMomento: l.precioUnitarioEnEseMomento,
-      )).toList();
+      final lineasModel = rawLines.map((l) => DetailLineModel.fromDrift(l)).toList();
 
       // Se une la cabecera con sus líneas usando RemissionModel
-      history.add(RemissionModel.fromSqlite(headers.toJson(), lineasModel));
+      history.add(RemissionModel.fromDrift(headers, lineasModel));
+
     }
 
     return history;
@@ -46,33 +40,14 @@ Future<List<RemissionModel>> obtenerHistorialRemisiones() async {
     // .transaction() asegura la integridad de la base de datos
     await _database.transaction(() async{
       //Se convierte el modelo a lo que entiende Drift
-      await _database.into(_database.remissions).insert(
-        db.RemissionsCompanion.insert(
-          id: remission.id,
-          folio: remission.folio,
-          fechaCreacion: remission.fechaCreacion,
-          nombreVendedor: remission.nombreVendedor,
-          nombreCliente: remission.nombreCliente,
-          firmaPath: remission.firmaPath,
-          fotoEvidenciaPath: Value(remission.fotoEvidenciaPath),// Value() permite nulos en Drift
-          )
-      );
+      await _database.into(_database.remissions).insert(remission.toCompanion());
 
       //Inserta cada uino de los renglones de detalle
       for(var lines in remission.detailLines){
-        final l = lines as DetailLineModel;
+        final lineModel = lines is DetailLineModel
+        ? lines : DetailLineModel.fromEntity(lines);
 
-        await _database.into(_database.detailLines).insert(
-          db.DetailLinesCompanion.insert(
-            id: l.id,
-            remisionId: l.remisionId,
-            productoId: l.prodctoId,
-            nombreProducto: l.nombreProducto,
-            cantidad: l.cantidad,
-            precioSugeridoOriginal: l.precioSugeridoOriginal,
-            precioUnitarioEnEseMomento: l.precioUnitarioEnEseMomento,
-          )
-        );
+        await _database.into(_database.detailLines).insert(lineModel.toCompanion());
       }
     });
   }
@@ -80,6 +55,20 @@ Future<List<RemissionModel>> obtenerHistorialRemisiones() async {
   Future<int> obtenerSiguienteFolio() async{
     final quantity = await _database.select(_database.remissions).get();
     return quantity.length + 1;
+  }
+
+  Future<RemissionModel?> getRemissionById(String id) async{
+    final remissionNow = await (_database.select(_database.remissions)
+    ..where((tbl) => tbl.id.equals(id))
+    ).getSingleOrNull();
+    if(remissionNow == null) return null;
+
+    final detailRow = await (_database.select(_database.detailLines)
+    ..where((d) => d.remisionId.equals(id))).get();
+
+    final linesModel = detailRow.map((l) => DetailLineModel.fromDrift(l)).toList();
+
+    return RemissionModel.fromDrift(remissionNow, linesModel);
   }
 
 }
